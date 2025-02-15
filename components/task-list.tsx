0de9710fa387e6fd06a3d9d9e9de67a2b835a8fb
BykaWF <yaroslavprozorov213@gmail.com>
2025-02-15 16:34:37 +0100

    Add Sidebar, Todo-list

---
+++ components/task-list.tsx
@@ -0,0 +1,16 @@
+import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "./ui/card"
+
+export default function TaskList(){
+    return(
+        <Card>
+            <CardHeader>
+                <CardTitle>Task List</CardTitle>
+                <CardDescription>List of tasks</CardDescription>
+            </CardHeader>
+            <CardContent>
+                
+            </CardContent>
+
+        </Card>
+    )
+}
